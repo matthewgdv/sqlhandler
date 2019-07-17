328875b115f467467ffa8b3235644fe007a6c93f
matthewgdv <matthewgdv@gmail.com>
2019-07-17 12:24:40 +0100

    Refactor config to use miscutils.NameSpace

--- sqlhandler/config.py
+++ sqlhandler/config.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from maybe import Maybe
 from subtypes import Enum
 from pathmagic import PathLike, File
+from miscutils import NameSpace
 
 from sqlhandler import localres
 
@@ -14,30 +15,22 @@ class Config:
         MS_SQL, MY_SQL, SQLITE, POSTGRESQL, ORACLE = "mssql", "mysql", "sqlite", "posgresql", "oracle"
 
     def __init__(self, path: PathLike = None) -> None:
-        self.resources = File.from_resource(localres, "config.json") if path is None else File.from_pathlike(path)
-        self.data = Maybe(self.resources.contents).else_({"default_host": None, "hosts": {}})
+        self.resources = File.from_resource(package=localres, name="config", extension="json") if path is None else File.from_pathlike(path)
+        self.data = Maybe(self.resources.contents).else_(NameSpace(default_host=None, hosts={}))
 
     def __repr__(self) -> str:
         return repr(self.data)
 
     def add_host(self, host: str, drivername: str, default_database: str, username: str = None, password: str = None, port: str = None, query: dict = None, is_default: bool = False) -> None:
-        self.data["hosts"][host] = {
-            "drivername": drivername,
-            "default_database": default_database,
-            "username": username,
-            "password": password,
-            "port": port,
-            "query": query,
-        }
-
+        self.data.hosts[host] = NameSpace(drivername=drivername, default_database=default_database, username=username, password=password, port=port, query=query)
         if is_default:
             self.set_default_host(host=host)
 
     def set_default_host(self, host: str) -> None:
-        if host in self.data["hosts"]:
-            self.data["default_host"] = host
+        if host in self.data.hosts:
+            self.data.default_host = host
         else:
-            raise ValueError(f"Host {host} is not one of the currently registered hosts: {', '.join(self.data['hosts'])}. Use {type(self).__name__}.add_host() first.")
+            raise ValueError(f"Host {host} is not one of the currently registered hosts: {', '.join(self.data.hosts)}. Use {type(self).__name__}.add_host() first.")
 
     def clear_config(self) -> None:
         self.data = None
@@ -47,10 +40,10 @@ class Config:
         self.resources.contents = self.data
 
     def generate_url(self, host: str = None, database: str = None) -> str:
-        host = Maybe(host).else_(self.data.get("default_host"))
-        host_settings = self.data["hosts"][host]
-        database = Maybe(database).else_(host_settings["default_database"])
-        return Url(drivername=host_settings["drivername"], username=host_settings["username"], password=host_settings["password"], host=host, port=host_settings["port"], database=database, query=host_settings["query"])
+        host = Maybe(host).else_(self.data.default_host)
+        host_settings = self.data.hosts[host]
+        database = Maybe(database).else_(host_settings.default_database)
+        return Url(drivername=host_settings.drivername, username=host_settings.username, password=host_settings.password, host=host, port=host_settings.port, database=database, query=host_settings.query.to_dict())
 
 
 class Url(URL):
