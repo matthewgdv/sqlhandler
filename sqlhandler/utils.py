286a73132437de74f343ff81a89a0fd6d6ba97fe
matthewgdv <matthewgdv@gmail.com>
2019-10-03 16:10:57 +0100

    Fix bug in literalstatement function

--- sqlhandler/utils.py
+++ sqlhandler/utils.py
@@ -130,6 +130,6 @@ def literalstatement(statement: Any, format_statement: bool = True) -> str:
     formatted = sqlparse.format(bound, reindent=True, wrap_after=1000) if format_statement else bound  # keyword_case="upper" (removed arg due to false positives)
 
     stage1 = Str(formatted).re.sub(r"\bOVER \(\s*", lambda m: m.group().strip()).re.sub(r"(?<=\n)([^\n]*JOIN[^\n]*)(\bON\b[^\n;]*)(?=[\n;])", lambda m: f"  {m.group(1).strip()}\n    {m.group(2).strip()}")
-    stage2 = stage1.re.sub(r"(?<=\bJOIN[^\n]+\n\s+ON[^\n]+\n(?:\s*AND[^\n]+\n)*)(AND[^\n]+)(?=[\n;])(?!\nAND)", lambda m: f"    {m.group(1).strip()}")
+    stage2 = stage1.re.sub(r"(?<=\bJOIN[^\n]+\n\s+ON[^\n]+\n(?:\s*AND[^\n]+\n)*?)(AND[^\n]+)(?=[\n;])", lambda m: f"    {m.group(1).strip()}")
 
     return str(stage2)
